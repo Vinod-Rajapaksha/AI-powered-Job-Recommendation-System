2107c8a78f7161b52f76e4ec4593322cb72f7567
Vinod Madhuranga <165497570+Vinod-Madhuranga@users.noreply.github.com>
2025-07-19 19:39:27 +0530

    Initial commit

---
+++ backend/src/main/java/com/system/JobRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRecommendationSystemApplication.class, args);
+    }
+
+}
