fcc60e289fcabfc9ab2d3503952233557ce38af7
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-07-26 22:49:03 +0530

    import : NgxSpinnerModule

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,NgxSpinnerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('frontend');
 }
