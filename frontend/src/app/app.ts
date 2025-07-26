d236e7693860901902a21921c89a0e928990953a
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-07-26 23:05:41 +0530

    import/add : ngx-spinner

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,15 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+  ngOnInit(): void {
+    this.spinner.show('initial-loader');
+
+    setTimeout(() => {
+      this.spinner.hide('initial-loader');
+    }, 1500);
+  }
+
 }
