0da213e4c617a206b2fdd306195cdc35ca1e9de9
Vinod Madhuranga <165497570+Vinod-Madhuranga@users.noreply.github.com>
2025-07-26 19:10:42 +0530

    modified : admin-routing

--- frontend/src/app/admin/admin-routing-module.ts
+++ frontend/src/app/admin/admin-routing-module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: Login },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
